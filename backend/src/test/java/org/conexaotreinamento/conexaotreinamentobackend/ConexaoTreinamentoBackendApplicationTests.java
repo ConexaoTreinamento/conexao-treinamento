8bb8222ea806b87a6184b61fbd62699029ff8e40
Santiago Firpo <santiagoifirpo4@gmail.com>
2025-08-08 00:53:11 -0300

    Initialized backend with open api

---
+++ backend/src/test/java/org/conexaotreinamento/conexaotreinamentobackend/ConexaoTreinamentoBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package org.conexaotreinamento.conexaotreinamentobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexaoTreinamentoBackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
