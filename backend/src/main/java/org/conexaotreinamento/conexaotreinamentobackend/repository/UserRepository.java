bab55dd4938bc3efe93f24f8895a1cbf4c94f4c3
Gabriel Domingues <gabrielleao.gd@gmail.com>
2025-09-04 17:02:27 -0300

    fix

--- backend/src/main/java/org/conexaotreinamento/conexaotreinamentobackend/repository/UserRepository.java
+++ backend/src/main/java/org/conexaotreinamento/conexaotreinamentobackend/repository/UserRepository.java
@@ -1,26 +1,23 @@
 package org.conexaotreinamento.conexaotreinamentobackend.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.conexaotreinamento.conexaotreinamentobackend.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByIdAndDeletedAtIsNull(UUID id);
-<<<<<<< HEAD
 
     
-    List<User> findAllByDeletedAtIsNull();
-=======
+    //List<User> findAllByDeletedAtIsNull();
         
     Page<User> findAllByDeletedAtIsNull(Pageable pageable);
->>>>>>> 4a4a4a0e65274bff2ef97451958a72647b054001
     
     Optional<User> findByEmailAndDeletedAtIsNull(String email);
     
