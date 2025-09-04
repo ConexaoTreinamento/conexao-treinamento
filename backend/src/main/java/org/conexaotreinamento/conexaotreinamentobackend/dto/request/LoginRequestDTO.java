5eaa7dd347bb81071777cf016cb03bbcda8cf159
Gabriel Domingues <gabrielleao.gd@gmail.com>
2025-09-04 17:20:52 -0300

    feature validate email with pattern and regex

--- backend/src/main/java/org/conexaotreinamento/conexaotreinamentobackend/dto/request/LoginRequestDTO.java
+++ backend/src/main/java/org/conexaotreinamento/conexaotreinamentobackend/dto/request/LoginRequestDTO.java
@@ -1,14 +1,19 @@
 package org.conexaotreinamento.conexaotreinamentobackend.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-
 public record LoginRequestDTO(
-    @NotBlank(message = "Email é obrigatório")
-    @Size(max = 120)
-    String email,
-    
-    @NotBlank(message = "Senha é obrigatória")
-    String password
-) {}
+        @NotBlank(message = "Email é obrigatório")
+        @Size(max = 120)
+        @Pattern(
+                regexp = "^[a-z0-9.]+@[a-z0-9]+\\.[a-z]+(\\.[a-z]+)?$",
+                message = "Email deve ter formato válido"
+        )
+        String email,
+        @NotBlank(message = "Senha é obrigatória")
+        String password
+        ) {
+
+}
