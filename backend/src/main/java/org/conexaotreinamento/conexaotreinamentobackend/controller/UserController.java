e3556207727ed964f933ecc2c5471c7163a58fbe
Gabriel Domingues <gabrielleao.gd@gmail.com>
2025-09-04 16:57:52 -0300

    fixing merge

--- backend/src/main/java/org/conexaotreinamento/conexaotreinamentobackend/controller/UserController.java
+++ backend/src/main/java/org/conexaotreinamento/conexaotreinamentobackend/controller/UserController.java
@@ -1,12 +1,5 @@
 package org.conexaotreinamento.conexaotreinamentobackend.controller;
 
-<<<<<<< HEAD
-import java.util.List;
-
-=======
-import jakarta.persistence.EntityListeners;
-import lombok.RequiredArgsConstructor;
->>>>>>> 4a4a4a0e65274bff2ef97451958a72647b054001
 import org.conexaotreinamento.conexaotreinamentobackend.dto.request.CreateUserRequestDTO;
 import org.conexaotreinamento.conexaotreinamentobackend.dto.response.UserResponseDTO;
 import org.conexaotreinamento.conexaotreinamentobackend.service.UserService;
@@ -21,13 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
 import jakarta.persistence.EntityListeners;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-=======
->>>>>>> 4a4a4a0e65274bff2ef97451958a72647b054001
 @RestController
 @RequestMapping("/users")
 @EntityListeners(AuditingEntityListener.class)
@@ -36,14 +25,9 @@ public class UserController {
 
     private final UserService userService;
 
-<<<<<<< HEAD
     // Delete and create task
-    @PostMapping("/create")
-    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody CreateUserRequestDTO createUserRequest) {
-=======
     @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@RequestBody CreateUserRequestDTO createUserRequest) {
->>>>>>> 0812b4c7be288aa3b57750fc9578111260fd0922
         UserResponseDTO userResponse = userService.createUser(createUserRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
