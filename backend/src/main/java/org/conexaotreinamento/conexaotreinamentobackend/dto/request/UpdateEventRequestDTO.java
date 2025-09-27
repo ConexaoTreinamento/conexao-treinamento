a3fcdf75b3b47ebb041661aa4d829d012a43ef01
Gabriel Domingues <gabrielleao.gd@gmail.com>
2025-09-26 21:55:54 -0300

    bugfix UpdateEventRequestDTO

--- backend/src/main/java/org/conexaotreinamento/conexaotreinamentobackend/dto/request/UpdateEventRequestDTO.java
+++ backend/src/main/java/org/conexaotreinamento/conexaotreinamentobackend/dto/request/UpdateEventRequestDTO.java
@@ -4,11 +4,13 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateEventRequestDTO(
-        @Size(max = 200) String name,
-        LocalDate date,
+        @NotBlank @Size(max = 200) String name,
+        @NotNull LocalDate date,
         LocalTime startTime,
         LocalTime endTime,
         @Size(max = 255) String location,
